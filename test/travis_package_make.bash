12a82ba79fc19a8f2d82aaa0ebaab26ba3d293e1
SnowMasaya <snow.akogi.pgel@gmail.com>
2019-04-30 03:00:34 +0000

    first commit

---
+++ test/travis_package_make.bash
@@ -0,0 +1,10 @@
+#!/bin/bash -xve
+
+rsync -av ./ ~/catkin_ws/src/pimouse_run_corridor/
+
+cd ~/catkin_ws/src/
+git clone --depth=l https://github.com/citueda/pimouse_ros.git
+
+
+cd ~/catkin_ws
+catkin_make
