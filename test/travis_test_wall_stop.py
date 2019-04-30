01b8c81cbc63cb816f7923ad04f0430ce2213598
SnowMasaya <snow.akogi.pgel@gmail.com>
2019-04-30 05:43:35 +0000

    update

--- test/travis_test_wall_stop.py
+++ test/travis_test_wall_stop.py
@@ -30,7 +30,7 @@ class WallStopTest(unittest.TestCast):
         self.assertTrue(left == 0 and right == 0, "can't move again")
         
 
-if __name__ = '__main__':
+if __name__ == '__main__':
    time.sleep(3)
    rospy.init_node('travis_test_wall_stop')
    rostest.rosrun('pimouse_run_corridor', 'travis_test_wall_stop', 'WallStopTest')
